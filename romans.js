bbde2e5418e36a01ce174399493c32f58d1273f6
genxhero <genxhero@users.noreply.github.com>
2019-11-22 17:01:13 -0800

    parse hundreds in js

--- romans.js
+++ romans.js
@@ -44,7 +44,8 @@ const parse_tens = (integer) => {
 }
 
 const parse_hundreds = (integer) => {
-
+    hash = { 0: "", 1: "C", 2: "CC", 3: "CCC", 4: "CD", 5: "D", 6: "DC", 7: "DCC", 8: "DCCC", 9: "CM" }
+    hash[integer]
 }
 
 const parse_thousands = (integer) => {
