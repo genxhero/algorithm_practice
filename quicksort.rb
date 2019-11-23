1aba269708d3dd13f562e0e09e87eb55ecdabd98
genxhero <genxhero@users.noreply.github.com>
2019-11-23 13:34:59 -0800

    write upt he partition and a test case

--- quicksort.rb
+++ quicksort.rb
@@ -1,10 +1,31 @@
 def quicksort(arr, first, last)
   if first < last
-    pivot = partition(arr, first, last)
-    quicksort(arr, first, pivot - 1)
-    quicksort(arr, pivot + 1, last)
+    index = partition(arr, first, last)
+    quicksort(arr, first, index - 1)
+    quicksort(arr, index + 1, last)
   end
-
   arr
 end
 
+def partition(arr, first, last)
+  pivot = arr[last]
+  index = first
+  
+  i = first
+  while i < last
+    if arr[i] <= pivot
+      temp = arr[i]
+      arr[i] = arr[index]
+      arr[index] = temp
+      index += 1
+    end
+    i += 1
+  end
+  temp = arr[index]
+  arr[index] = pivot
+  arr[last] = temp
+  return index
+end
+
+test_arr_a = [4, 1, 10, 6, 7, 2]
+puts quicksort(test_arr_a, test_arr_a.first, test_arr_a.last).inspect
