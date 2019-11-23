a75c8a23641519857ad971a663be2c335455f6e1
genxhero <genxhero@users.noreply.github.com>
2019-11-22 16:43:17 -0800

    parse hundreds subroutine

--- romans.rb
+++ romans.rb
@@ -20,6 +20,8 @@ def parse_tens(integer)
 end
 
 def parse_hundreds(integer)
+    hash = { 0 => "", 1 => "C", 2 => "CC", 3 => "CCC", 4 => "CD", 5 => "D", 6 => "DC", 7 => "DCC", 8 => "DCCC", 9 => "CM"}
+    hash[integer]
 end
 
 def parse_thousands(integer)
