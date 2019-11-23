63b1c4f9991434997dd73ef80b4231249c8452df
genxhero <genxhero@users.noreply.github.com>
2019-11-22 16:50:57 -0800

    add functionality to turn an integer into array of integer digits

--- romans.js
+++ romans.js
@@ -1,11 +1,12 @@
 
 const romanize = (integer) => {
     let result = "";
-
+    const digits = `${integer}`.split("").map(digit => parseInt(digit))
 }
 
 const parse_ones = (integer) => {
-
+    const hash = {0: "", 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8:"VIII", 9: "IX"}
+    return hash[integer]
 }
 
 const parse_tens = (integer) => {
