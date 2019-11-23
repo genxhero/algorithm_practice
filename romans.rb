f8a90742946b2dd9882ff40b61b2276285e043a4
genxhero <genxhero@users.noreply.github.com>
2019-11-22 16:35:52 -0800

    logic for roman numeral main function

--- romans.rb
+++ romans.rb
@@ -1,5 +1,12 @@
 def romanize_number(integer)
-
+    result = ""
+    digits = integer.digits.reverse
+    length = digits.length
+    return result if length == 0
+    result += parse_thousands(digits[0]) if length == 4
+    result += parse_hundreds(digits[length-3]) if length >= 3
+    result += parse_tens(digits[length-2]) if length >= 2
+    result += parse_tens(digits[length-1]) if length >= 2
 end
 
 def parse_ones(integer)
@@ -9,4 +16,7 @@ def parse_tens(integer)
 end
 
 def parse_hundreds(integer)
+end
+
+def parse_thousands(integer)
 end
