204153cb74039e4211d9154956a6f8549dec9b1d
genxhero <genxhero@users.noreply.github.com>
2019-11-22 16:47:18 -0800

    start javascript version of romanizer

---
+++ romans.js
@@ -0,0 +1,21 @@
+
+const romanize = (integer) => {
+    let result = "";
+
+}
+
+const parse_ones = (integer) => {
+
+}
+
+const parse_tens = (integer) => {
+
+}
+
+const parse_hundreds = (integer) => {
+
+}
+
+const parse_thousands = (integer) => {
+
+}
