e9e25db7bd4bbcd0f16af8b02b98a5da39c7828c
genxhero <genxhero@users.noreply.github.com>
2019-11-22 16:38:14 -0800

    parse ones subroutine part 2

--- romans.rb
+++ romans.rb
@@ -11,6 +11,7 @@ end
 
 def parse_ones(integer)
     hash = { 0 => "", 1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "IV", 6 => "VI", 7 => "VII", 8 => "VIII", 9 => "IX"}
+    return hash[integer]
 end
 
 def parse_tens(integer)
