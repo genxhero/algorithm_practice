12a915aca860b819a9cce600809b06c7e25c7802
genxhero <genxhero@users.noreply.github.com>
2019-11-22 16:55:19 -0800

    if logic for main romanizer

--- romans.js
+++ romans.js
@@ -1,7 +1,29 @@
 
 const romanize = (integer) => {
     let result = "";
-    const digits = `${integer}`.split("").map(digit => parseInt(digit))
+    const digits = `${integer}`.split("").map(digit => parseInt(digit));
+    const length = digits.length;
+    if (length === 0) {
+        return ""
+    }  
+    
+    if (length === 4) {
+        result += parse_thousands(digits[0]);
+    }  
+    
+    if (length >= 3) {
+        result += parse_hundreds(digits[length-3]);
+    }
+
+    if (length >= 2) {
+        result += parse_tens(digits[length-2]);
+    }
+
+    if (length >= 2) {
+        result += parse_ones(digits[length-1]);
+    }
+
+    return result;
 }
 
 const parse_ones = (integer) => {
