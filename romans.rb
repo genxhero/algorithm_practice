8d35533707e1f8e7eef9f940ec69d06747ec62e1
genxhero <genxhero@users.noreply.github.com>
2019-11-22 16:37:50 -0800

    parse ones subroutine

--- romans.rb
+++ romans.rb
@@ -10,6 +10,7 @@ def romanize_number(integer)
 end
 
 def parse_ones(integer)
+    hash = { 0 => "", 1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "IV", 6 => "VI", 7 => "VII", 8 => "VIII", 9 => "IX"}
 end
 
 def parse_tens(integer)
