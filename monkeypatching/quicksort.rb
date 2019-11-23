c93960f08cf5677c8e465f1b27dcec8b8af9fa3e
genxhero <genxhero@users.noreply.github.com>
2019-11-23 13:44:41 -0800

    add monkey patch version of ruby quicksort

---
+++ monkeypatching/quicksort.rb
@@ -0,0 +1,35 @@
+class Array
+    def quicksort(self, first = 0, last = self.length-1)
+        if first < last
+            index = partition(arr, first, last)
+            self.quicksort(self, first, index - 1)
+            self.quicksort(self, index + 1, last)
+        end
+        self
+    end
+
+private
+    def partition(arr, first, last)
+    pivot = arr[last]
+    index = first
+    
+    i = first
+    while i < last
+        if arr[i] <= pivot
+        temp = arr[i]
+        arr[i] = arr[index]
+        arr[index] = temp
+        index += 1
+        end
+        i += 1
+    end
+    temp = arr[index]
+    arr[index] = pivot
+    arr[last] = temp
+    return index
+    end
+
+end
+
+unsorted = [10, 4, 37, 2, 5, 21, 8, 11, 3]
+puts unsorted.quicksort
