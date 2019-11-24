6a2b20e020e7395c3a9a966dccfa0af72afd75a5
genxhero <genxhero@users.noreply.github.com>
2019-11-24 11:29:28 -0800

    rename method to be more clear

--- math.rb
+++ math.rb
@@ -2,8 +2,8 @@ def fibonacci(n)
 end
 
 #Returns the sum of the first n fibonacci numbers
-def fibs_sum(n)
+def fibonacci_sum(n)
     return 0 if n == 0 #base case 1
     return 1 if n == 1 #base case 2
-    fibs_sum(n-1) + fibs_sum(n-2) + 1
+    fibonacci_sum(n-1) + fibonacci_sum(n-2) + 1
 end
