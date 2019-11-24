ad6d847562409b3117f229228f55ff290188af55
genxhero <genxhero@users.noreply.github.com>
2019-11-24 11:28:49 -0800

    fix typo

--- monkeypatching/mergesort.rb
+++ monkeypatching/mergesort.rb
@@ -10,5 +10,18 @@ class Array
 
     private 
     def self.merge(left, right, &proc)
+        result = [];
+        until left.empty? || right.empty?
+            case proc.call(left.first, right.first)
+            when -1
+                result << left.shift
+            when 0, 1
+                result << right.shift
+            end
+        end
+        result + left + right
     end
 end
+
+unsorted = [10, 4, 37, 2, 5, 21, 8, 11, 3]
+puts unsorted.merge_sort.inspect
