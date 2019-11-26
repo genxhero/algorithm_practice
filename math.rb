365a5c209fdeeed55f6927c738cdde6485867464
genxhero <genxhero@users.noreply.github.com>
2019-11-26 09:49:37 -0800

    factorial

--- math.rb
+++ math.rb
@@ -15,5 +15,12 @@ def fibonacci_sum(n)
     fibonacci_sum(n-1) + fibonacci_sum(n-2) + 1
 end
 
+#doing this from memory could be wrong
+def factorial(num)
+    (1..num).inject(:*) 
+end
+
+
 puts "The first eight fibonacci numbers are: #{fibonacci(8)}"
 puts "If you don't see 0, 1, 1, 2, 3, 5, 8, 13 then the code isn't working."
+puts "Factorial 4 should be 24.  Result: #{factorial(4)}"
