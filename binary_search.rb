b80ace7df7bb1a8176f3ddb04543a1ebb806ac06
Aaron Goddard <genxhero@users.noreply.github.com>
2019-11-26 11:17:14 -0800

    binary search

---
+++ binary_search.rb
@@ -0,0 +1,11 @@
+def binary_search(arr, target)
+    return nil if arr.length <= 1
+     mid = arr.length / 2
+     return mid if arr[mid] == target
+     if arr[mid] > target
+       return binary_search(arr.take(mid), target)
+     else
+       result = binary_search(arr.drop(mid + 1), target)
+       result.nil? ?  nil : mid + result + 1
+     end
+end
