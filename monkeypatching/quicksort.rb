cee6b90294d09a0b9f55bf9b63a863bbf6f6323f
Aaron Goddard <genxhero@users.noreply.github.com>
2019-11-26 11:04:28 -0800

    my quick sort

--- monkeypatching/quicksort.rb
+++ monkeypatching/quicksort.rb
@@ -9,6 +9,15 @@ class Array
         self
     end
 
+    def my_quick_sort(&prc)
+        prc ||= proc {|a, b| a<=>b}
+        return self if size < 2
+        pivot = first
+        left = self[1..-1].select{|el| prc.call(el, pivot) == -1}
+        right = self[1..-1].select{|el| prc.call(el, pivot) != -1}
+        left.my_quick_sort(&prc) + [pivot] + right.my_quick_sort(&prc)
+      end
+
 private
     def partition(arr, first, last)
     pivot = arr[last]
