cb458eb88055f0a362513770c2ee1c55784a2330
Aaron Goddard <genxhero@users.noreply.github.com>
2019-11-27 11:05:45 -0800

    ceasar cypher main code

--- caesar.rb
+++ caesar.rb
@@ -3,6 +3,12 @@
 def caesar(string, offset)
     arr = string.split('')
     result = []
-    library = ("a".."z").to_a
-    encoded = library.rotate
+    alphabet = ("a".."z").to_a
+    encoded = alphabet.rotate(offset)
+    library = {" ": " ", ".": ".", "!": "!", ",": ","}
+    alphabet.each_with_index do |letter, i| 
+        library[letter] = encoded[i]
+    end
+    result = arr.map{|el| library[el] }
+    result.join('')
 end
