3d0662e4235555e8118820cf6a987347df56c634
Aaron Goddard <genxhero@users.noreply.github.com>
2019-11-27 11:31:15 -0800

    array rotate function

--- caesar.js
+++ caesar.js
@@ -1,9 +1,14 @@
 import {ALPHABET} from './constants';
 
-
+const rotation = (offset) => {
+    const left = ALPHABET.slice(offset);
+    const right = ALPHABET.slice(0, offset);
+    return left.concat(right);
+}
 
 const caesar = (string, offset) => {
     array = string.split('')
     const library = {' ': ' ', '.':'.', ',': ',', '!': '!', '-': '-'};
-    
+    const encoded = rotation(offset);
+
 }
