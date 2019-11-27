1ed2e911c8e68741d8d29cc98115db87fd3c511d
Aaron Goddard <genxhero@users.noreply.github.com>
2019-11-27 11:38:24 -0800

    actually export the lousy library from constants.js...works better that way.

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const ALPHABET = ["a","b","c","d","e",
+export const ALPHABET = ["a","b","c","d","e",
                 "f","g","h", "i","j",
                 "k","l","m","n","o","p",
                 "q","r","s","t","u","v","w","x","y","z"]
