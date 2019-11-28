3c29b7c1f37328ea97b3d681489b6962398aa460
genxhero <genxhero@users.noreply.github.com>
2019-11-28 07:27:26 -0800

    fix float string problem

--- binary_search.js
+++ binary_search.js
@@ -2,12 +2,12 @@ const  binary_search = (arr, target) => {
      if (arr.length <= 1) {
          return null
      }
-     let mid = arr.length / 2
+     let mid = parseInt(arr.length / 2)
     if (arr[mid] === target) {
         return mid
     }
      if (arr[mid] > target) {
-         return binary_search(arr.slice(mid), target)
+         return binary_search(arr.slice(0, mid), target)
      } else { 
          result = binary_search(arr.slice(mid, arr.length), target)
      }
@@ -15,3 +15,5 @@ const  binary_search = (arr, target) => {
 }
 
 console.log(binary_search([1, 3, 5, 7, 9], 5))
+console.log(binary_search([1, 3, 5, 7, 9], 7))
+console.log(binary_search([1, 3, 5, 7, 9], 21))
