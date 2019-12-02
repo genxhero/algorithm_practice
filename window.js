c0667d06d40fe3c501bd55479080b0366c49a689
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-02 07:53:43 -0800

    string perms

--- window.js
+++ window.js
@@ -1,3 +1,28 @@
 /**
  * This is the "find how many permutations of a little string are inside of a big string" problem
+ * Utilies the "window" solution
  */
+
+ const findPermutations = (string) => {
+    const results = [];
+
+    if (string.length === 1) 
+    {
+      results.push(string);
+      return results;
+    }
+
+    for (var i = 0; i < string.length; i++) 
+    {
+      const firstChar = string[i];
+      const otherChar = string.substring(0, i) + string.substring(i + 1);
+      const otherPermutations = getPermutations(otherChar);
+
+      for (var j = 0; j < otherPermutations.length; j++) {
+        results.push(firstChar + otherPermutations[j]);
+      }
+    }
+    return results;
+ }
+
+ console.log(findPermutations("yes")) 
