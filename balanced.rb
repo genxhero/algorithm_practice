44550e49d7df8cb0068a1ea6d37db393715e32bb
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-06 15:40:03 -0800

    oops forgot this was ruby

--- balanced.rb
+++ balanced.rb
@@ -1,12 +1,12 @@
 def balanced?(string)
      openers = {"(": ")", "[": "]", "{": "}"};
      closers = {")": "(", "]": "[", "}": "{"};
-     return false if closers[string[0]] || string.length < 2 || string.length % 2 !== 0  
+     return false if closers[string[0]] || string.length < 2 || string.length % 2 != 0  
      stack = []
      string.split('').each do |char|
         stack << char if openers[char]
         if closers[char]
-            (stack.last !== openers[char] || stack.length === 0) ? return false : stack.pop
+            (stack.last != openers[char] || stack.length == 0) ? return false : stack.pop
         end
      end
      return true
