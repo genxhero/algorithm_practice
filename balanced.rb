3afa2d3fc0b180fcae1a748dabefdb26b14b118b
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-06 15:33:38 -0800

    main loop

--- balanced.rb
+++ balanced.rb
@@ -2,5 +2,7 @@ def balanced?(string)
      openers = {"(": ")", "[": "]", "{": "}"};
      closers = {")": "(", "]": "[", "}": "{"};
      return false if closers[string[0]] || string.length < 2 || string.length % 2 !== 0  
-    
+     stack = []
+     string.split('').each do |elem|
+     end
 end
