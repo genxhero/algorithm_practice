e85f30b92e2aef86176a4271bb971e78aa1b07c9
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-05 23:27:53 -0800

    fix mis-directed inequality

--- balanced.js
+++ balanced.js
@@ -1,7 +1,7 @@
 const isBalanced = (string) => {
     const openers = {"(": ")", "[": "]", "{":"}"};
     const closers = {")": "(", "]": "[", "}": "{"};
-    if (closers[string[0]] || string.length > 2 || string.length % 2 !== 0)  {
+    if (closers[string[0]] || string.length < 2 || string.length % 2 !== 0)  {
         return false;
     }
 
@@ -23,4 +23,4 @@ const isBalanced = (string) => {
 }
 
 console.log(isBalanced("(){}[]"))
-console.log(isBalanced("")
+console.log(isBalanced("]["))
