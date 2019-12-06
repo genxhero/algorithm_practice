3a7f8fcd8adc24b5dbfd8d2bc3b0f5cb17d91953
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-06 15:34:21 -0800

    change elem to char

--- balanced.rb
+++ balanced.rb
@@ -3,6 +3,6 @@ def balanced?(string)
      closers = {")": "(", "]": "[", "}": "{"};
      return false if closers[string[0]] || string.length < 2 || string.length % 2 !== 0  
      stack = []
-     string.split('').each do |elem|
+     string.split('').each do |char|
      end
 end
