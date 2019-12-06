76e2a644e3785f7dbb354a0c780a27f91173bb6e
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-06 15:31:58 -0800

    edge case early return

--- balanced.rb
+++ balanced.rb
@@ -1,4 +1,6 @@
 def balanced?(string)
      openers = {"(": ")", "[": "]", "{": "}"};
      closers = {")": "(", "]": "[", "}": "{"};
+     return false if closers[string[0]] || string.length < 2 || string.length % 2 !== 0  
+    
 end
