1692e828c7f265a105ab64cd31f194c67c258bf1
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-06 16:15:18 -0800

    call to sym on the chars so that they will actually look up the hash

--- balanced.rb
+++ balanced.rb
@@ -1,17 +1,18 @@
 def balanced?(string)
      openers = {"(": ")", "[": "]", "{": "}"};
      closers = {")": "(", "]": "[", "}": "{"};
-     return false if closers[string[0]] || string.length < 2 || string.length % 2 != 0  
+     return false if closers[string[0].to_sym] or string.length < 2 or string.length % 2 != 0  
      stack = []
      string.split('').each do |char|
-        stack << char if openers[char]
+        stack << char if openers[char.to_sym]
+        puts stack.inspect
         if closers[char]
-            (stack.last != openers[char] || stack.length == 0) ? return false : stack.pop
+            (stack.last != openers[char.to_sym] || stack.length == 0) ? (return false) : stack.pop
         end
      end
      return true
 end
 
-puts balanced?("(){}[]")
+# puts balanced?("(){}[]")
 puts balanced?("][")
-puts balanced?("{{[]}}")
+# puts balanced?("{{[]}}")
