ef4f4a74c390c64c466a7039838d14a3bc9d0823
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-07 23:19:08 -0800

    move logger

--- window.rb
+++ window.rb
@@ -9,8 +9,8 @@
     (0...string.length).each do |i| 
         firstChar = string[i];
         otherChar = string[0..i] + string[(i + 1)..-1];
+        puts otherChar
         otherPermutations = permutations(otherChar);
-        puts otherPermutations.inspect
 
         (0...therPermutations.length).each do |j| 
             results << (firstChar + otherPermutations[j]);
