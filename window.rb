add6fc458e6b4ba0a5120d0606cd66f4375bcac2
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-07 23:07:10 -0800

    clean up the code

--- window.rb
+++ window.rb
@@ -2,20 +2,17 @@
     results = [];
 
     if (string.length == 1) 
-    {
       results << string
       return results;
-    }
+    end
 
-    for ( i = 0; i < string.length; i++) 
-    {
-       firstChar = string[i];
-       otherChar = string[0..i] + string[i + 1];
-       otherPermutations = permutations(otherChar);
-
-      for (let j = 0; j < otherPermutations.length; j++) {
-        results << (firstChar + otherPermutations[j]);
-      }
-    }
+    (0...string.length).each do |i| 
+        firstChar = string[i];
+        otherChar = string[0..i] + string[i + 1];
+        otherPermutations = permutations(otherChar);
+        (0...therPermutations.length).each do |j| 
+            results << (firstChar + otherPermutations[j]);
+        end
+    end
     results
 end
