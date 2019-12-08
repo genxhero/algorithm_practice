b7a2edf076fe2ee06680e5711153a0698708a02c
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-07 23:25:05 -0800

    change rest of variables to snake case

--- window.rb
+++ window.rb
@@ -8,11 +8,11 @@
 
     (0...string.length).each do |i| 
         first_char = string[i];
-        otherChar = string[0...i] + string[(i + 1)..-1];
-        otherPermutations = permutations(otherChar);
+        other_char = string[0...i] + string[(i + 1)..-1];
+        other_permutations = permutations(other_char);
 
-        (0...otherPermutations.length).each do |j| 
-            results << (first_char + otherPermutations[j]);
+        (0...other_permutations.length).each do |j| 
+            results << (first_char + other_permutations[j]);
         end
     end
     results
