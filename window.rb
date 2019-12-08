e7ab5fd95ca9d13686bd010a24b70d03a7511942
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-07 23:03:46 -0800

    stage 2

--- window.rb
+++ window.rb
@@ -1,18 +1,15 @@
  def permutations (string)
-    const results = [];
+    results = [];
 
-    if (string.length === 1) 
+    if (string.length == 1) 
     {
-      results.push(string);
+      results << string
       return results;
     }
 
-    for (let i = 0; i < string.length; i++) 
+    for ( i = 0; i < string.length; i++) 
     {
       const firstChar = string[i];
-      //i === 1, substring 0, i would be b.
-      // substring i + 1 (2) would make it "tt"
-      //so otherChar of butt would be btt
       const otherChar = string.substring(0, i) + string.substring(i + 1);
       const otherPermutations = findPermutations(otherChar);
 
