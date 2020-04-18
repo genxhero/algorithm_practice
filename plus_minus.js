638bf78ec80945ba841e896b51bdeb85f8267621
genxhero <genxhero@users.noreply.github.com>
2020-04-18 15:42:00 -0700

    plussy minussy

---
+++ plus_minus.js
@@ -0,0 +1,14 @@
+const plusMinus = (arr) => {
+    const denominator = arr.length;
+    let negatives = 0;
+    let zeroes = 0;
+    let positives = 0;
+    for(let index = 0; index<arr.length; index++) {
+        if (arr[index] === 0) { zeroes += 1 }
+if (arr[index] > 0) { positives += 1 }
+if (arr[index] < 0) { negatives += 1 }
+    }
+console.log(positives / denominator)
+console.log(negatives / denominator)
+console.log(zeroes / denominator)
+}
