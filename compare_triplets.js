287fcfea7519472a3911caa983c45ba37e5b693f
genxhero <genxhero@users.noreply.github.com>
2020-04-18 15:11:20 -0700

    cmpare triplets

---
+++ compare_triplets.js
@@ -0,0 +1,12 @@
+const compareTriplets = (a, b) => {
+    let aScore = 0;
+    let bScore = 0;
+    for (let index = 0; index < 3; index++) {
+        if (a[index] > b[index]) {
+            aScore += 1
+        } else if (a[index] < b[index]) {
+            bScore += 1
+        }
+    }
+    return [aScore, bScore]
+}
