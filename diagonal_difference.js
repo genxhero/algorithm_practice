8bb9f4955ebdcfe10023ce21b0d7d63de0812603
genxhero <genxhero@users.noreply.github.com>
2020-04-18 15:33:04 -0700

    actual diagonal arr thing

---
+++ diagonal_difference.js
@@ -0,0 +1,29 @@
+const  diagonalDifference = (arr) => {
+    const diagA = getTopDownDiagonal(arr)
+    const diagB = getBottumUpDiagonal(arr)
+    return Math.abs(diagA - diagB)
+}
+
+const getTopDownDiagonal = (arr) => {
+    let row = 0;
+    let col = 0;
+    let sum = 0
+    while (row < arr.length) {
+        sum += arr[row][col]
+        row += 1;
+        col += 1
+    }
+    return sum;
+}
+
+const getBottumUpDiagonal = (arr) => {
+    let row = arr.length - 1;
+    let col = 0;
+    let sum = 0;
+    while (row >= 0) {
+        sum += arr[row][col]
+        row -= 1;
+        col += 1
+    }
+    return sum;
+}
