d74de2592b8eb9916d42842339ab7ddff5a12bbb
genxhero <genxhero@users.noreply.github.com>
2020-04-19 10:00:33 -0700

    subsequences

--- subsequences.js
+++ subsequences.js
@@ -14,4 +14,39 @@ const getSubsequences = arr => {
     return subs;
 }
 
-console.log(getSubsequences([2, 1, 3]))
+const getStrictlyIncreasing = arr => {
+    let maxLength = 1;
+    let maxCount = 0;
+    for (let i =0; i < arr.length; i++) {
+        const sorted = isSorted(arr[i])
+            if (sorted ) {
+                if (arr[i].length === maxLength) {
+                    maxCount += 1
+                } else if (arr[i].length > maxLength) {
+                    console.log("Bigger length, resetting", arr[i])
+                    maxCount = 1;
+                    maxLength = arr[i].length
+                }
+            }
+    }
+    return `Max Length: ${maxLength}, Count: ${maxCount}`
+}
+
+const isSorted = arr => {
+    if (arr.length <= 1) {
+        return true;
+    }
+    for (let i=0; i < arr.length; i++) {
+       if (arr[i] > arr[i + 1]) {
+           return false
+       }   
+    }
+    return true
+}
+
+
+// console.log(getSubsequences([2, 1, 3]))
+
+const subbies = getSubsequences([2, 3, 1])
+
+console.log(getStrictlyIncreasing(subbies))
