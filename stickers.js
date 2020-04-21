651a94e2198db791b0152ea44693e5163f02ad88
genxhero <genxhero@users.noreply.github.com>
2020-04-21 13:14:46 -0700

    all tests pass

--- stickers.js
+++ stickers.js
@@ -1,8 +1,9 @@
 const stickers_for = (phrase) => {
-    const arr = phrase.split('')
+    const arr = phrase.replace(/\s+/g, '').split('')
     let stickersNeeded = 1;
     let charCount = {}
-    let maxKey = "i"
+    let maxKey = arr[0];
+
     for (let i = 0; i < arr.length; i++) {
         charCount[arr[i]] ? charCount[arr[i]] += 1 : charCount[arr[i]] = 1
         if (charCount[maxKey] < charCount[arr[i]] && arr[i] !== ' ') {
@@ -10,9 +11,10 @@ const stickers_for = (phrase) => {
         }
     }
     if (charCount[maxKey] > 1) {
-        maxKey === 'a' ? stickersNeeded += ((charCount[maxKey] - 2) / 2) : stickersNeeded += (charCount[maxKey] - 1)
+        maxKey === 'a' ? stickersNeeded += parseInt(((charCount[maxKey] - 2) / 2)) : stickersNeeded += (charCount[maxKey] - 1)
     }
-    console.log(stickersNeeded)
+
+    return stickersNeeded
 }
 
-stickers_for("artisan martians")
+console.log(stickers_for("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "));
