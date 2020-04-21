3326e0beed9eee9bcef46ebf17faa2a380225060
genxhero <genxhero@users.noreply.github.com>
2020-04-21 12:23:17 -0700

    fizzbuzz

---
+++ palindrome_substring.js
@@ -0,0 +1,12 @@
+const findSubstrings = string => {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length + 1; j++) {
+            result.push(str.slice(i, j));
+        }
+    }
+    return result;
+}
+
+const longestPalindromeSubstring = string => {
+    const subs = findSubstrings(string);
+}
