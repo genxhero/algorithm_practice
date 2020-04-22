709715fe50d4077e200756151fb606d7c4496f3e
genxhero <genxhero@users.noreply.github.com>
2020-04-22 09:03:35 -0700

    anagrams

--- anagrams.js
+++ anagrams.js
@@ -1,18 +1,27 @@
 const funWithAnagrams = (text) => {
+
     const newArr = [];
+    const wordHash = {}
 
     for (let i = 0; i < text.length; i++) {
+        console.log("text len", text.length, "I:", i)
         let current = text[i]
-        for (let j = 1; j < text.length - 1; j++) {
-            console.log("current:", current, "testing against", text[j])
-            current.split('').sort().join('') === text[j].split('').sort().join('')
-            if (current.split('').sort().join('') === text[j].split('').sort().join('')) {
-                console.log("splicing")
-                text.splice(j, 1)
-            }
+        if (!wordHash[current.split('').sort().join('')] ) {
+            wordHash[current.split('').sort().join('')] = true
+            newArr.push(current)
         }
-        return text;
+        console.log("Current", current)
+        for (let j = i + 1; j < text.length; j++) {
+            console.log("current:", current, "testing against", text[j], "j", j)
+            current.split('').sort().join('') === text[j].split('').sort().join('')
+            if (!current.split('').sort().join('') === text[j].split('').sort().join('')) {
+                //  console.log("splicing")
+                newArr.push(text[j])
+            } 
+        }
     }
+    return newArr.sort();
+
 }
 
 console.log(funWithAnagrams(["code", "doce", "ecod", 'framer', 'frame']))
