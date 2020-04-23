7ed1a7bf7b83a799461a4e8dabf9147f942a8e68
genxhero <genxhero@users.noreply.github.com>
2020-04-22 21:08:09 -0700

    palindromic substring

---
+++ longest_palindrome.rb
@@ -0,0 +1,9 @@
+def longest_palindrome(s)
+    return "" if s == ""
+    return s if s.reverse == s
+    arr = s.chars
+    s.length.downto(1) do |n|
+    longest = arr.each_cons(n).find { |sub| sub == sub.reverse }
+    return longest.join if longest
+  end
+end
