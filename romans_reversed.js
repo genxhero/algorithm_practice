8a32862954eeb051a7f638cb71449b95c4276974
genxhero <genxhero@users.noreply.github.com>
2020-04-22 22:00:17 -0700

    reverse romans

--- romans_reversed.js
+++ romans_reversed.js
@@ -1,13 +1,4 @@
 const romanToInteger = roman => {
-    // const numeralHash = {
-    //     "M": 1000,
-    //     "V": 5,
-    //     "I": 1,
-    //     "X": 10,
-    //     "D": 500,
-    //     "L": 50,
-    //     "C": 100
-    // }
 
     let result = 0;
     for (let i=0; i < roman.length; i++) {
@@ -17,35 +8,63 @@ const romanToInteger = roman => {
         switch (current) {
             case "M": {
                 prev === "C" ? result += 900 : result += 1000
+                break;
             }
 
             case "D": {
-                prev === "C" ? result += 400 : result += 500
+              prev === "C" ? result += 400 : result += 500
+                break;
             }
 
             case "C": {
-                next === "M" ? result += 0 : result += 100
+                if (next === "M") {
+                    console.log("Next is M, adding nothing.")
+                    result += 0
+                    break;
+                } else if(prev === "X")  {
+                    console.log("Prev was x, adding 90")
+                        result += 90
+                        break;
+                    } else {
+                        console.log("neither x nor m involved, adding 100")
+                        result += 100
+                        break;
+                }
             }
 
             case "L": {
                 prev === "X" ? result += 40 : result += 50
+                break;
             }
 
             case "X": {
-                prev === "I" ? result += 9 : result += 10
+                if (prev === "I" ) {
+                    result += 9
+                    break;
+                } else if (next === "C") {
+                    result += 0
+                    break;
+                } else {
+                    result += 10
+                    break;
+                }
             }
 
             case "V": {
-                prev === "I" ? result += 4 : result += 5
+               prev === "I" ? result += 4 : result += 5
+               break;
             }
 
             case "I": {
                 (next === "V" || next === "X") ? result += 0 : result += 1
+                break;
             }
         }
     }
     return result;
 }
 
-console.log(romanToInteger("MM"))
-console.log(romanToInteger("MCMLXXXV")) //should be 1985, it's gonna not be because the code above is wrong.
+// console.log(romanToInteger("MM"))
+// console.log(romanToInteger("MCMLXXXV")) //1985
+// console.log(romanToInteger("MCMLXXXIV"))  //1984
+console.log(romanToInteger("MCMXCV")) //should be 1995 but because of the case with C it won't work.
