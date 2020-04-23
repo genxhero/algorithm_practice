5ff2a0ee81690dd7b4744c989c73a7c1dd081e69
genxhero <genxhero@users.noreply.github.com>
2020-04-22 21:33:28 -0700

    convert to case statement.

--- romans_reversed.js
+++ romans_reversed.js
@@ -1,19 +1,55 @@
 const romanToInteger = roman => {
-    const numeralHash = {
-        "M": 1000,
-        "V": 5,
-        "I": 1,
-        "X": 10,
-        "D": 500,
-        "L": 50,
-        "C": 100
-    }
+    // const numeralHash = {
+    //     "M": 1000,
+    //     "V": 5,
+    //     "I": 1,
+    //     "X": 10,
+    //     "D": 500,
+    //     "L": 50,
+    //     "C": 100
+    // }
+
     let result = 0;
     for (let i=0; i < roman.length; i++) {
-        result += numeralHash[roman[i]]
+        let current = roman[i]
+        let prev = roman[i-1]
+        let next = roman[i+1]
+        switch (current) {
+            case "M": {
+                prev === "C" ? result += 900 : result += 1000
+            }
+
+            case "D": {
+
+            }
+
+            case "D": {
+
+            }
+
+            case "C": {
+                next === "M" ? result += 0 : result += 100
+            }
+
+            case "L": {
+
+            }
+
+            case "X": {
+
+            }
+
+            case "V": {
+
+            }
+
+            case "I": {
+
+            }
+        }
     }
     return result;
 }
 
 console.log(romanToInteger("MM"))
-console.log("MCMLXXV") //should be 1985, it's gonna not be because the code above is wrong.
+console.log(romanToInteger("MCMLXXXV")) //should be 1985, it's gonna not be because the code above is wrong.
