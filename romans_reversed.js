8fed09caf962f412aa54b2ffbb1cb4c4cfd7cfed
genxhero <genxhero@users.noreply.github.com>
2020-04-22 22:17:21 -0700

    fix typo

--- romans_reversed.js
+++ romans_reversed.js
@@ -15,32 +15,36 @@ const romanToInteger = roman => {
                 break;
             }
             case "C": {
-                if (next === "M" || next === "D") {
-                    result += 0
-                    break;
-                } else if(prev === "X")  {
-                        result += 90
-                        break;
-                    } else {
-                        result += 100
-                        break;
-                }
+                // if (next === "M" || next === "D") {
+                //     result += 0
+                //     break;
+                // } else if(prev === "X")  {
+                //         result += 90
+                //         break;
+                //     } else {
+                //         result += 100
+                //         break;
+                // }
+                result += handleC(prev, next );
+                break;
             }
             case "L": {
                 prev === "X" ? result += 40 : result += 50
                 break;
             }
             case "X": {
-                if (prev === "I" ) {
-                    result += 9
-                    break;
-                } else if (next === "C" || next === "L") {
-                    result += 0
-                    break;
-                } else {
-                    result += 10
-                    break;
-                }
+                // if (prev === "I" ) {
+                //     result += 9
+                //     break;
+                // } else if (next === "C" || next === "L") {
+                //     result += 0
+                //     break;
+                // } else {
+                //     result += 10
+                //     break;
+                // }
+                result += handleX(prev, next);
+                break;
             }
             case "V": {
                prev === "I" ? result += 4 : result += 5
@@ -55,9 +59,29 @@ const romanToInteger = roman => {
     return result;
 }
 
-// console.log(romanToInteger("MM"))
-// console.log(romanToInteger("MCMLXXXV")) //1985
-// console.log(romanToInteger("MCMLXXXIV"))  //1984
+const handleX = (prev, next) => {
+    if (prev === "I") {
+        return  9
+    } else if (next === "C" || next === "L") {
+        return 0
+    } else {
+        return 10
+    }
+}
+
+const handleC = (prev, next) => {
+    if (next === "M" || next === "D") {
+        return 0
+    } else if (prev === "X") {
+        return 90
+    } else {
+        return 100
+    }
+}
+
+ console.log(romanToInteger("MM"))
+ console.log(romanToInteger("MCMLXXXV")) //1985
+ console.log(romanToInteger("MCMLXXXIV"))  //1984
 console.log(romanToInteger("MCMXCV")) //1995
 console.log(romanToInteger("MCDLXXVI")) //1476
 console.log(romanToInteger("MMMXLV")) //Should be 3045, is 3055
