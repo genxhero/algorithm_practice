ff4cba612157d8071e8f7595ba568aaee75d2e75
genxhero <genxhero@users.noreply.github.com>
2020-04-22 21:16:33 -0700

    first iteration of reverse romans

---
+++ romans_reversed.js
@@ -0,0 +1,16 @@
+const romanToInteger = roman => {
+    const numeralHash = {
+        "M": 1000,
+        "V": 5,
+        "I": 1,
+        "X": 10,
+        "D": 500,
+        "L": 50
+    }
+    let result = 0;
+    for (let i=0; i < roman.length; i++) {
+        result += numeralHash[roman[i]]
+    }
+}
+
+console.log(romanToInteger("MM"))
