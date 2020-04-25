b08d72d5990eb8788e51737b36346e5ade09b317
genxhero <genxhero@users.noreply.github.com>
2020-04-24 20:40:20 -0700

    wait, did that just work?

--- longest_common_prefix.rb
+++ longest_common_prefix.rb
@@ -3,8 +3,12 @@
 def longest_common_prefix(strings)
     result = ""
     shortest = strings.min_by(&:length)
+    (0..shortest.length).each do |i|
+        result += shortest[i] if strings.all? {|string| string[i] == shortest[i]}
+    end
     puts strings.inspect
     puts shortest
+    puts result
     result
 end
 
