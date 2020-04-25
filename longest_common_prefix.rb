7cd4d63393e16061c89d2e75c02379954e052609
genxhero <genxhero@users.noreply.github.com>
2020-04-24 20:40:48 -0700

    neat

--- longest_common_prefix.rb
+++ longest_common_prefix.rb
@@ -6,10 +6,7 @@ def longest_common_prefix(strings)
     (0..shortest.length).each do |i|
         result += shortest[i] if strings.all? {|string| string[i] == shortest[i]}
     end
-    puts strings.inspect
-    puts shortest
-    puts result
     result
 end
 
-longest_common_prefix(["flow", "flower", "florist", "flour"]) #Should be "flo"
+puts longest_common_prefix(["flow", "flower", "florist", "flour"]) #Should be "flo"
