2ff191f8d64050ebc084264444db347224baebb8
genxhero <genxhero@users.noreply.github.com>
2020-04-27 10:48:13 -0700

    solve the daggone thing

--- three_sum.rb
+++ three_sum.rb
@@ -1,6 +1,10 @@
 def three_sum(array)
     perms = array.permutation(3).to_a
-    result = perms.select {|perm| perm.inject(:+) == 0}
+    result = [] 
+    perms.each do |perm|
+        result << perm.sort if !result.include?(perm.sort) && perm.inject(:+) == 0
+    end
+    result
 end
 
 puts three_sum([-1, 0, 1, 2, -1, -4]).inspect
