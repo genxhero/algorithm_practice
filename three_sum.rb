01abc498e9a1c33c039166bf04577758836ba492
genxhero <genxhero@users.noreply.github.com>
2020-04-27 10:45:10 -0700

    get permutations of 3, select those that add to 0

--- three_sum.rb
+++ three_sum.rb
@@ -1,8 +1,9 @@
 def three_sum(array)
-    
+    perms = array.permutation(3).to_a
+    result = perms.select {|perm| perm.inject(:+) == 0}
 end
 
-puts three_sum([-1, 0, 1, 2, -1, -4])
+puts three_sum([-1, 0, 1, 2, -1, -4]).inspect
 
 #Should return...
 # [
