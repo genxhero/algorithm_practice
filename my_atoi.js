fa924a64380973e897512d9d69f27ee0475bb768
genxhero <genxhero@users.noreply.github.com>
2020-05-03 14:44:15 -0700

    array to integer

--- my_atoi.js
+++ my_atoi.js
@@ -1,4 +1,6 @@
 
+//Array to integer
+
 const myAtoi = str => {
 
         const numerals = { 0: true, 1: true, 2: true, 3: true, 4: true, 5: true, 6: true, 7: true, 8: true, 9: true, "-": true, "+": true };
