c900df0d0bab6f3692d272d7c7cbd933c0037925
genxhero <genxhero@users.noreply.github.com>
2020-05-11 08:11:21 -0700

    thing

--- most_living.js
+++ most_living.js
@@ -1,3 +1,24 @@
-const mostLiving = (startYear, endYear) => {
+
+const people = [
+    {
+        born: 1824,
+        died: 1900
+    },
+    {
+        born: 1823,
+        died: 1885
+    },
+    {
+        born: 1878,
+        died: 1898
+    }
+]
+
+
+
+const mostLiving = (people) => {
+
+    const years = []
+
 
 }
