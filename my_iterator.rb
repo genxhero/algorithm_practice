d6e456435bfce85c273405df044dfca1d56768a6
genxhero <genxhero@users.noreply.github.com>
2020-05-12 19:31:37 -0700

    start weird iterator problem

---
+++ my_iterator.rb
@@ -0,0 +1,3 @@
+#Given K sorted (ascending) arrays with N elements in each array, implement
+#an iterator for iterating over the elements of the arrays in ascending order.
+#[[1,5,7], [2,3,10],[4,6,9]] = 1, 2, 3, 4, 5, 6, 7, 8, 9
