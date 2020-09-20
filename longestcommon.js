512c890a0c86d67284bd98c49facaf8e944ceb50
genxhero <genxhero@users.noreply.github.com>
2020-09-20 08:31:24 -0700

    blah

---
+++ longestcommon.js
@@ -0,0 +1,27 @@
+const user0 = ["/start", "/green", "/blue", "/pink", "/register", "/orange", "/one/two"];
+var user1 = ["/start", "/pink", "/register", "/orange", "/red", "a"];
+var user2 = ["a", "/one", "/two"];
+var user3 = ["/pink", "/orange", "/yellow", "/plum", "/blue", "/tan", "/red", "/amber", "/HotRodPink", "/CornflowerBlue", "/LightGoldenRodYellow", "/BritishRacingGreen"];
+var user4 = ["/pink", "/orange", "/amber", "/BritishRacingGreen", "/plum", "/blue", "/tan", "/red", "/lavender", "/HotRodPink", "/CornflowerBlue", "/LightGoldenRodYellow"];
+var user5 = ["a"];
+
+const findContiguousHistory = (userA, userB) => {
+  const subArrayA = findSubArrays(userA);
+  const subArrayB = findSubArrays(userB);
+  // [ {key: ["/start", "/green", "/blue"], value: 3 } ]
+  const hasBoth = [];
+  for (i < 0; i < subArrayB.length; i++) {
+    if (subArrayA.includes(subArrayB[i])) {
+      hasBoth.push(subArrayB[i])
+    }
+  }
+  return findMaxByLength(hasBoth);
+}
+
+const findSubArrays = array => {
+    //Finds sub arrays of various lengths using the window method
+}
+
+// const findMaxByLength = arrayOfObjects => {
+//   for (let i)
+// }
