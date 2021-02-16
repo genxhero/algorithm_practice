f56b07341f1acd7345ae02b88b5895d37aab125b
genxhero <genxhero@users.noreply.github.com>
2021-02-16 13:36:14 -0800

    start own isbalanced

---
+++ balanced_capitals.js
@@ -0,0 +1,7 @@
+const isBalanced = (string) => {
+    let hash = {};
+    
+}
+
+console.log("AbBa should be balanced (expect true): ", isBalanced("AbBa"))
+console.log("AbBaC should not be balanced (expect false): ", isBalanced("AbBaC"))
