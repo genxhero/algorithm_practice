ea3761f0902e121cc9d38ec0e9dc0e1753cf7043
genxhero <genxhero@users.noreply.github.com>
2021-02-16 13:41:52 -0800

    my balancing

--- balanced_capitals.js
+++ balanced_capitals.js
@@ -1,6 +1,15 @@
 const isBalanced = (string) => {
     let hash = {};
-    
+    for (let i = 0; i < string.length; i++) {
+        hash[string[i]] = true;
+    }
+
+    for (let j = 0; j < string.length; j++) {
+        if (!hash[string[j].toUpperCase()]  || !hash[string[j].toLowerCase()]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 console.log("AbBa should be balanced (expect true): ", isBalanced("AbBa"))
